d5b495fcc54f21cc9a4da50087117e78d8dba1c1
Longinus <jesuscaesar@outlook.com>
2021-12-28 21:19:14 +1000

    Latest update

--- control.php
+++ control.php
@@ -37,8 +37,12 @@ macOS Control Center
 </div>
 <div class='panel'>
 <p align="center">
+<label>Background host: </label>
+<input type='text' id='setBackFieldHost' style="width:25%;position:relative;" placeholder="Filename or URL" value="<?=explode('/', $background)[2];?>">
+<input type='text' id='setBackFieldUser' style="width:25%;position:relative;" placeholder="Filename or URL" value="<?=explode('/', $background)[3];?>"><br>
 <label>Background image: </label>
-<input type='text' id='setBackField' placeholder="Filename or URL" value="<?=$background;?>">
+<input type='text' id='setBackFieldRepo' style="width:25%;position:relative;" placeholder="Filename or URL" value="<?=explode('/', $background)[4];?>">
+<input type='text' id='setBackField' style="width:25%;position:relative;" placeholder="Filename or URL" value="<?=str_replace('back.', '', str_replace('.png?raw=true', '', explode('/', $background)[7]));?>">
 </p>
 </div>
 </body>
