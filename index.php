d02468f3c63a0482e46a04d9fbcf55ba58f82451
Nestor Barone <jesuscaesar@outlook.com>
2022-04-27 02:15:49 +0000

    Add new functionality

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ foreach ($list as $key=>$value) {
 ?>
 <img class="hover" style="height:15%;position:relative;" title="<?=$fileTitle;?>" src="<?=$fileIcon;?>?rev=<?=time();?>" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" onclick="<?=$fileLink;?>">
 <?php } ?>
-<img class="hover" style="height:15%;position:relative;" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" title="Exit" src="sys.exit.png?rev=<?=time();?>" onclick="window.location.href = '../';">
+<img class="hover" style="height:15%;position:relative;" onmouseover="playAudio(soundPlayer, '<?=$soundlib[rand(0,$soundct)];?>?rev=<?=time();?>');" title="Exit" src="sys.exit.png?rev=<?=time();?>" onclick="get('r','','mac','hsis','','flossely',false);">
 </p>
 </div>
 <audio id="soundPlayer" <?php if (!$sounds) { ?>muted="muted"<?php } ?>>
